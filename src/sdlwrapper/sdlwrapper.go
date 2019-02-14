6a5ad38783be419af3b3369d67c16f96efb331a4
Kiril Kostov <kkostovk@gmail.com>
2019-02-14 21:41:35 +0200

    Add sdlwrapper comments.

--- src/sdlwrapper/sdlwrapper.go
+++ src/sdlwrapper/sdlwrapper.go
@@ -1,3 +1,4 @@
+// Package sdlwrapper provides a simple wrapper for SDL.
 package sdlwrapper
 
 import (
@@ -7,11 +8,14 @@ import (
 	"github.com/veandco/go-sdl2/sdl"
 )
 
+// Defines a wrapper for sdls window and renderer.
 type DisplayWrapper struct {
 	window   *sdl.Window
 	renderer *sdl.Renderer
 }
 
+// Create and initialize a new DisplayWrapper with the given width, height and name.
+// Return a pointer to the DisplayWrapper and nil error if everything is successful.
 func NewDisplayWrapper(width, height int, name string) (*DisplayWrapper, error) {
 	err := sdl.Init(sdl.INIT_EVERYTHING)
 	if err != nil {
@@ -34,22 +38,26 @@ func NewDisplayWrapper(width, height int, name string) (*DisplayWrapper, error) 
 	return &display, nil
 }
 
+// Destroy the sdl window and renderer, and call sdl.Quit().
 func (d *DisplayWrapper) Destroy() {
 	d.renderer.Destroy()
 	d.window.Destroy()
 	sdl.Quit()
 }
 
+// Draw a pixel with the given color at the given coordinates.
 func (d *DisplayWrapper) DrawPixel(x, y int, color utils.Color) {
 	r, g, b := color.ToRGB()
 	d.renderer.SetDrawColor(r, g, b, 255)
 	d.renderer.DrawPoint(int32(x), int32(y))
 }
 
+// Display the pixels that have been set.
 func (d *DisplayWrapper) Display() {
 	d.renderer.Present()
 }
 
+// Wait for an exit event.
 func WaitExit() {
 	running := true
 	for running {
