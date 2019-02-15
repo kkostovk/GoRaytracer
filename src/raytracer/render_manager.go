3af81e72e922b9bcc738c9d9b43f4535daad5e10
Kiril Kostov <kkostovk@gmail.com>
2019-02-15 21:46:09 +0200

    Add some semple scenes.

--- src/raytracer/render_manager.go
+++ src/raytracer/render_manager.go
@@ -2,8 +2,8 @@
 package raytracer
 
 import (
-	"GoRaytracer/src/mathutils"
 	"GoRaytracer/src/utils"
+	"fmt"
 	"sync"
 )
 
@@ -35,8 +35,8 @@ func NewRenderManager() RenderManager {
 }
 
 // Setup sets up the current RenderManager from a scene file.
-func (r *RenderManager) Setup(_fileName string) {
-	r.dummySceneSetup()
+func (r *RenderManager) Setup(fileName string) {
+	r.setupScene(fileName)
 }
 
 // Render prepares and starts the rendering.
@@ -109,34 +109,59 @@ func (r *RenderManager) raytrace(ray *Ray) utils.Color {
 	return utils.NewColor(255, 255, 255)
 }
 
-func (r *RenderManager) dummySceneSetup() {
+func (r *RenderManager) setupScene(fileName string) {
+	sceneReader, err := NewSceneReader(fileName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Read frame width and height
+	frameWidth, frameHeight, err := sceneReader.GetFrameSettings()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	r.frameWidth = frameWidth
+	r.frameHeight = frameHeight
+
 	r.frameWidth = 640
 	r.frameHeight = 480
 	r.vfb = make([][]utils.Color, r.frameHeight)
 	for row := range r.vfb {
 		r.vfb[row] = make([]utils.Color, r.frameWidth)
 	}
-	// Setup camera
-	camera := NewParallelCamera(mathutils.NewVector(60, 60, -100), 0, 30, 0, 90, float64(r.frameWidth)/float64(r.frameHeight))
-	r.camera = &camera
+
 	scene := NewScene()
 	r.scene = &scene
-	scene.SetAmbientLight(utils.NewColor(255, 255, 255))
-	light1 := NewLight(mathutils.NewVector(35, 180, -100), utils.NewColor(255, 255, 255), 25000)
-	scene.AddLight(light1)
-	// Add scene nodes
 
-	// Add a plane
-	plane := NewPlane(mathutils.NewVector(0, 0, 0), 300, 1)
-	planeColor := NewSimpleColor(utils.NewColor(0, 0, 255))
-	planeShader := NewLambert(utils.Color{0, 0, 0}, &planeColor)
-	scene.AddNode(&plane, &planeShader)
+	// Read the camera
+	camera, err := sceneReader.GetCamera()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	r.camera = &(camera)
 
-	// Add a sphere
-	sphere := NewSphere(mathutils.NewVector(0, 70, 0), 20)
-	sphereColor := NewSimpleColor(utils.NewColor(255, 255, 0))
-	//sphereChecker := raytracer.NewChecker(utils.NewColor(0, 255, 0), utils.NewColor(255, 0, 0), 50)
-	//sphereShader := raytracer.NewPhong(utils.Color{0, 255, 0}, &sphereColor, 5.3, 20)
-	sphereShader := NewLambert(utils.Color{0, 255, 0}, &sphereColor)
-	scene.AddNode(&sphere, &sphereShader)
+	// Read ambient light
+	r.scene.ambientLight, err = sceneReader.GetAmbientLight()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Read lights
+	r.scene.lights, err = sceneReader.GetLights()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Read scene nodes
+	r.scene.SceneNodes, err = sceneReader.GetSceneNodes()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
